369cbd1317b398a4ef893b90a55d2e2dfdececa6
puhaiyang <puhaiyang@seanet.com.cn>
2024-11-06 16:52:11 +0800

    init

---
+++ src/main/java/com/haiyang/ai/SpringAiDemoApplication.java
@@ -0,0 +1,34 @@
+package com.haiyang.ai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringAiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiDemoApplication.class, args);
+    }
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        return new SimpleVectorStore(embeddingModel);
+    }
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+}
