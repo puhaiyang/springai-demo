6a04e70b112349978241152ec8983d8a56e1e1f3
puhaiyang <761396462@qq.com>
2024-11-10 19:34:41 +0800

    add etl

--- src/main/java/com/haiyang/ai/controller/AIController.java
+++ src/main/java/com/haiyang/ai/controller/AIController.java
@@ -3,23 +3,29 @@ package com.haiyang.ai.controller;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class AIController {
 
     private final OllamaChatModel chatModel;
+    private final VectorStore vectorStore;
 
     @Autowired
-    public AIController(OllamaChatModel chatModel) {
+    public AIController(OllamaChatModel chatModel, VectorStore vectorStore) {
         this.chatModel = chatModel;
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/ai")
@@ -31,5 +37,13 @@ public class AIController {
     public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         Prompt prompt = new Prompt(new UserMessage(message));
         return this.chatModel.stream(prompt);
+    }
+
+    @GetMapping("/ai/vectorStoreSearch")
+    public Map<String, String> getSystem(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        List<Document> similarDocuments = this.vectorStore.similaritySearch(message);
+        String documents = similarDocuments.stream().map(Document::getContent)
+                .collect(Collectors.joining(System.lineSeparator()));
+        return Map.of("result", documents);
     }
 }
